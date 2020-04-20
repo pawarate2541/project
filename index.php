576edf918c69813c9ef44720b858b71f19d01fde
pawarate2541 <63330983+pawarate2541@users.noreply.github.com>
2020-04-20 10:26:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'ouhqskdRP/sUP8uwpjAadPDJz6rj1Y3IR0/ZznmHBgsPmYq6Q+hzdEJ4OXgyw/8NaLy6GLAZYYbLhF/7S6i8K07k3yxT0sWcMEa6ixgJ2c0XIOEKRfUEQAsHVi4PbQU4HEk9GOq/cmdR3iRkQE9e5gdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = 'ba6e01c3eb0671a32e7d9fb3dbabd67d';
+$ACCESS_TOKEN = 'WqFC4QmBYDd5ZYutXd3ZMKS2zizMZWHc7PzS1W/Xt3//C/6s8nCUn8/2SzCu2SyBFcEe7vy/VAckgL7P32dhcOqalm5wpO91T4YkplUYh6WwSbP+cWCmHntm2TzH/hD1oNLWLDAZqb4JULuuq8lErQdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = '98fdde1d2a1e51e0b7c0cd2472d69002';
 
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
