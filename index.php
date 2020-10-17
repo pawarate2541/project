e862895c207a6f9161157e0bca8c2d6a5707bb9a
pawarate2541 <63330983+pawarate2541@users.noreply.github.com>
2020-10-17 18:06:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,9 +13,7 @@ $request_array = json_decode($request, true);   // Decode JSON to Array
 
 
 $jsonFlex = [
-    "type" => "flex",
-    "altText" => "Hello Flex Message",
-    "contents" => [
+   
       "type": "template",
   "altText": "this is a buttons template",
   "template": [
@@ -34,7 +32,7 @@ $jsonFlex = [
         "text": "Action 2"
     ]
     ]
-]
+
 ]
   ];
 
